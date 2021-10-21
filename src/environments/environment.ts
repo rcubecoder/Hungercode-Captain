660b1809b184e4ff1046eb9632cfe1bcc3c966f9
ravi-gajera27 <gajeraravi42@gmail.com>
2021-10-21 10:10:30 +0530

    commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,17 @@
 export const environment = {
   production: false,
- API: { url: 'http://localhost:5000/api/captain/' },
- // url: 'http://192.168.0.106:5000/api/user/'},
- //API: { url: 'https://firestep-node.herokuapp.com/api/user/' },
+  API: { url: 'http://localhost:5000/api/captain/' },
+  // url: 'http://192.168.0.106:5000/api/user/'},
+  //API: { url: 'https://firestep-node.herokuapp.com/api/user/' },
   firebaseConfig: {
-    apiKey: "AIzaSyBuDqvaSo06ctJVFpn-AG9gHow4HdxyAlQ",
-    authDomain: "peraket-rms.firebaseapp.com",
-    projectId: "peraket-rms",
-    storageBucket: "peraket-rms.appspot.com",
-    messagingSenderId: "122401619157",
-    appId: "1:122401619157:web:e99258dce12873fd27765c",
-    measurementId: "G-2J5TEVD94B"
+    apiKey: 'AIzaSyBuDqvaSo06ctJVFpn-AG9gHow4HdxyAlQ',
+    authDomain: 'peraket-rms.firebaseapp.com',
+    projectId: 'peraket-rms',
+    storageBucket: 'peraket-rms.appspot.com',
+    messagingSenderId: '122401619157',
+    appId: '1:122401619157:web:e99258dce12873fd27765c',
+    measurementId: 'G-2J5TEVD94B',
   },
-  token_secret: 'THIs Is my SeCrEt',
+  qr_secret: 'THIs Is my SeCrEt',
   enc_secret: 'gstf78ijagf9hf09',
-}
+};
